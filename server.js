b7f07a60c75b94678d4f17318466cf54a7ec15e4
antonhansel <antoninribeaud@gmail.com>
2015-02-09 15:28:09 +0100

    Fetching results from various status pages

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // server.js
-var config 		 = require('./lib/config/config.json');
 // set up ======================================================================
 // get all the tools we need
 var express  	 = require('express');
@@ -7,7 +6,6 @@ var app      	 = express();
 var port     	 = process.env.PORT || 8080;
 var morgan       = require('morgan');
 var path 		 = require('path');
-
 // configuration ===============================================================
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -18,6 +16,7 @@ app.use(express.static(path.join(__dirname, 'public'))); // to get local files
 
 // routes ======================================================================
 require('./lib/app/routes.js')(app); // load our routes and pass in our app
+require('./lib/app/socketHandler.js');
 
 // launch ======================================================================
 app.listen(port);
