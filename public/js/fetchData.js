2350fd0fbb5925a957357289e94705721c793576
antonhansel <antoninribeaud@gmail.com>
2015-02-10 10:56:46 +0100

    Cleaning repository by removing libs and adding them to bower

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -1,11 +1,32 @@
+var urlsList = null;
+
 jQuery(function() {
+  $("#slider").slider({
+    value: 100,
+    min: 0,
+    max: 1000,
+    step: 50,
+    slide: function(event, ui) {
+      $("#slider-value").html(ui.value);
+    }
+  });
+
+  $("#slider-value").html($('#slider').slider('value'));
+
   var socket = io('http://localhost:8081');
+
   socket.on('data', function(res) {
-    console.log(res);
     var date = new Date().getTime();
     updateGraphs(res, date);
   });
+
   socket.on('urls', function(urls) {
+    if(urlsList) {
+      for(var i = 0; i < urls.length; i++) {
+        $('.flot-' + urls[i][0]).remove();
+      }
+    }
+    urlsList = urls;
     initGraphs(urls);
   });
 });
--- public/js/updateGraphs.js
+++
@@ -1,8 +0,0 @@
-/* 
-* @Author: antonhansel
-* @Date:   2015-02-09 18:15:01
-* @Last Modified by:   antonhansel
-* @Last Modified time: 2015-02-09 18:15:01
-*/
-
-'use strict';
