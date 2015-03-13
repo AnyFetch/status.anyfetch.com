eb9ca02cd07e04c46ea4a972aa97cedcf356a86b
antonhansel <antoninribeaud@gmail.com>
2015-03-13 10:31:42 +0100

    Refactoring

--- public/js/fetch-data.js
+++ public/js/fetch-data.js
@@ -1,5 +1,4 @@
 var urlsList = null;
-var storedValues = 100;
 
 $(function() {
   if(source === 'global') {
@@ -15,6 +14,7 @@ function initSocket(socketSource) {
   var socket = io.connect();
 
   socket.source = socketSource;
+  var graph = new Graph(socket.source);
   socket.on('reconnect', function() {
     socket.emit('source', socketSource);
   });
@@ -22,47 +22,47 @@ function initSocket(socketSource) {
   socket.emit('source', socketSource);
   socket.on('data', function(res) {
     var date = new Date().getTime();
-    updateGraphs(res, date, true, socket.source);
+    graph.updateGraphs(res, date, true);
   });
 
   socket.on('urls', function(urls) {
     if(urlsList) {
-      dataArray = [];
-      warnings = [];
+      graph.dataArray = [];
+      graph.warnings = [];
       $('#grid-mode').show();
       $('#justify-mode').hide();
       $('#graph-container').empty();
       $('#global-graph').empty();
     }
     urlsList = urls;
-    initGraphs(urls);
+    graph.initGraphs(urls);
   });
-}
 
-//
-// Changing grid mode
-//
+  //
+  // Changing grid mode
+  //
 
-$('#grid-mode').hide();
-
-$("#grid-mode").click(function() {
   $('#grid-mode').hide();
-  $('#justify-mode').show();
-  $('#graph-container').empty();
-  for(var i = 0; i < dataArray.length; i++) {
-    var html = '';
-    html += '<div class=col-md-4>';
-    html += dataArray[i].html;
-    html += '</div>';
-    $('#graph-container').append(html);
-  }
-});
 
-$("#justify-mode").click(function() {
-  $('#justify-mode').hide();
-  $('#grid-mode').show();
-  $('#graph-container').empty();
-  for(var i = 0; i < dataArray.length; i++) {
-    $('#graph-container').append(dataArray[i].html);
-  }
-});
+  $("#grid-mode").click(function() {
+    $('#grid-mode').hide();
+    $('#justify-mode').show();
+    $('#graph-container').empty();
+    for(var i = 0; i < graph.dataArray.length; i++) {
+      var html = '';
+      html += '<div class=col-md-4>';
+      html += graph.dataArray[i].html;
+      html += '</div>';
+      $('#graph-container').append(html);
+    }
+  });
+
+  $("#justify-mode").click(function() {
+    $('#justify-mode').hide();
+    $('#grid-mode').show();
+    $('#graph-container').empty();
+    for(var i = 0; i < graph.dataArray.length; i++) {
+      $('#graph-container').append(graph.dataArray[i].html);
+    }
+  });
+};
--- public/js/graph.js
+++ public/js/graph.js
@@ -1,122 +1,128 @@
-var dataArray = [];
-var warnings = [];
-var meaningFulData = {
-  hydraters: 'pending',
-  providers: 'pending_documents'
-};
+'use strict';
 
-var totalDocuments = 0;
-var globalFlot = {
+var Graph = function Graph(source) {
+  this.storedValues = 100;
+  this.source = source;
+  this.dataArray = [];
+  this.warnings = [];
+  this.meaningFulData = {
+    hydraters: 'pending',
+    providers: 'pending_documents'
+  };
+
+  this.totalDocuments = 0;
+  this.globalFlot = {
     name: 'Global Statistics',
     dataSet: [{label: "Documents pending", data: [], color: "#00FF00"}],
     id: 'global-statistics',
-    options: getOptions(),
+    options: this.getOptions(),
     html: "<div class='panel panel-default'><div class='panel-heading'>" +
     'Global statistics' + "</div><div id=flot-" + 'global-statistics' +
     " style='height:300px;'></div></div>"
+  };
 };
 
-function initGraphs(providers) {
-  $('#global-graph').append(globalFlot.html);
+Graph.prototype.initGraphs = function(providers) {
+  $('#global-graph').append(this.globalFlot.html);
 
   $.each(providers, function(index, value) {
     var dataSet = {
-      name: index.charAt(0).toUpperCase() + index.slice(1) + ' ' + source + ' - ' + value,
+      name: index.charAt(0).toUpperCase() + index.slice(1) + ' ' + this.source + ' - ' + value,
       dataSet: [{label: "Documents pending", data: [], color: "#00FF00"}],
       id: index,
-      options: getOptions(),
+      options: this.getOptions(),
       html: ''
     };
-    dataSet.html = generateHtml(dataSet);
+    dataSet.html = this.generateHtml(dataSet);
     // $('#graph-container').append(dataSet.html);
     var html = '';
     html += '<div class=col-md-4>';
     html += dataSet.html;
     html += '</div>';
     $('#graph-container').append(html);
-    dataArray.push(dataSet);
-  });
-}
+    this.dataArray.push(dataSet);
+  }.bind(this));
+};
 
-function updateAll() {
-  $.plot($('#flot-global-statistics'), globalFlot.dataSet, globalFlot.options);
-  dataArray.forEach(function(elem) {
+Graph.prototype.updateAll = function() {
+  $.plot($('#flot-global-statistics'), this.globalFlot.dataSet, this.globalFlot.options);
+  this.dataArray.forEach(function(elem) {
     $.plot($('#flot-' + elem.id), elem.dataSet, elem.options);
   });
-}
+};
 
-function generateHtml(dataSet) {
+Graph.prototype.generateHtml = function(dataSet) {
   return ("<div class='panel panel-default'><div class='panel-heading'>" +
     dataSet.name + "</div><div id=flot-" + dataSet.id +
     " style='height:300px;'></div></div>");
-}
+};
 
-function updateGraphs(data, date, realtime, socketSource) {
-  totalDocuments = 0;
-  dataArray.forEach(function(item) {
-    if(item.options.yaxis.max < data[item.id][meaningFulData[socketSource]]) {
-      item.options.yaxis.max = data[item.id][meaningFulData[socketSource]];
+Graph.prototype.updateGraphs = function(data, date, realtime) {
+  this.totalDocuments = 0;
+  this.dataArray.forEach(function(item) {
+    if(item.options.yaxis.max < data[item.id][this.meaningFulData[this.source]]) {
+      item.options.yaxis.max = data[item.id][this.meaningFulData[this.source]];
     }
-    while(item.dataSet[0].data.length > storedValues) {
+    while(item.dataSet[0].data.length > this.storedValues) {
       item.dataSet[0].data.shift();
     }
-    if(data[item.id][meaningFulData[socketSource]] !== undefined) {
-      totalDocuments += [date, data[item.id][meaningFulData[socketSource]]][1];
-      item.dataSet[0].data.push([date, data[item.id][meaningFulData[socketSource]]]);
+    if(data[item.id][this.meaningFulData[this.source]] !== undefined) {
+      this.totalDocuments += [date, data[item.id][this.meaningFulData[this.source]]][1];
+      item.dataSet[0].data.push([date, data[item.id][this.meaningFulData[this.source]]]);
     }
     else {
-      if(!warnings.some(function(warning) {
+      if(!this.warnings.some(function(warning) {
         if(warning[0] === item.name) {
           return true;
         }
       })) {
-        warnings.push([item.name, 5]);
+        this.warnings.push([item.name, 5]);
       }
     }
-  });
+  }.bind(this));
 
-  globalFlot.dataSet[0].data.push([new Date().getTime(), totalDocuments]);
-  if(globalFlot.options.yaxis.max < totalDocuments) {
-    globalFlot.options.yaxis.max = totalDocuments;
+  this.globalFlot.dataSet[0].data.push([new Date().getTime(), this.totalDocuments]);
+  if(this.globalFlot.options.yaxis.max < this.totalDocuments) {
+    this.globalFlot.options.yaxis.max = this.totalDocuments;
   }
-  while(globalFlot.dataSet[0].data.length > storedValues) {
-    globalFlot.dataSet[0].data.shift();
+  while(this.globalFlot.dataSet[0].data.length > this.storedValues) {
+    this.globalFlot.dataSet[0].data.shift();
   }
-  for(var j = warnings.length - 1; j > -1; j--) {
-    if(warnings[j][1] > 0) {
-      warnings[j][1] -= 1;
+  for(var j = this.warnings.length - 1; j > -1; j--) {
+    if(this.warnings[j][1] > 0) {
+      this.warnings[j][1] -= 1;
     }
     else {
-      warnings.splice(j, 1);
+      this.warnings.splice(j, 1);
     }
   }
-  if (realtime) {
-    showWarnings();
-    updateAll();
+  if(realtime) {
+    this.showWarnings();
+    this.updateAll();
   }
-}
+};
 
-function showWarnings(){
+Graph.prototype.showWarnings = function(){
   $('#activity').empty();
   $('#unresponding').empty();
-  $('#unresponding').append('<p>' + warnings.length + ' not Responding:</p>');
-  warnings.forEach(function(item) {
+  $('#unresponding').append('<p>' + this.warnings.length + ' not Responding:</p>');
+  this.warnings.forEach(function(item) {
     $('#unresponding').append(item[0] + '</br>');
   });
   var total = 0;
-  dataArray.forEach(function(item) {
+  this.dataArray.forEach(function(item) {
     if(item.dataSet[0].data[item.dataSet[0].data.length - 1][1]) {
       total += item.dataSet[0].data[item.dataSet[0].data.length - 1][1];
     }
   });
   $('#activity').append('<p>' + total + ' document' + (total > 1 ? 's' : '') + ' pending:</p>');
-  dataArray.forEach(function(item) {
+  this.dataArray.forEach(function(item) {
     $('#activity').append(
       item.dataSet[0].data[item.dataSet[0].data.length - 1][1] + ' - ' + item.id + '</br>');
   });
-}
+};
 
-function getOptions() {
+Graph.prototype.getOptions = function() {
   var options = {
     series: {
       lines: {
@@ -166,4 +172,4 @@ function getOptions() {
     }
   };
   return options;
-}
+};
